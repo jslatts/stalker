8a31c632c3c9d8db8da7fd410e1d890654b25f98
Justin Slattery <justin.slattery@fzysqr.com>
2011-06-04 11:36:33 -0700

    More unit tests. Restructured watcher lib

--- spec/stalker-spec.js
+++ spec/stalker-spec.js
@@ -1,5 +1,5 @@
 describe('stalker', function () {
-  it('should call call back with an error when path is not found', function () {
+  it('watch should call call back with an error when path is not found', function () {
     var s = require('../lib/stalker');
 
     var error;
@@ -13,7 +13,7 @@ describe('stalker', function () {
 
     waitsFor(function () {
       return error;
-    }, 'error was not set', 200);
+    }, 'error to be set', 200);
 
     runs(function() {
       expect(error).toMatch(/Path does not exist/);
@@ -22,10 +22,9 @@ describe('stalker', function () {
 });
 
 describe('watcher', function () {
-  it('should call back with an error when path is not found', function () {
+  it('addFile should call back with an error when path is not found', function () {
     var s = require('../lib/watcher').makeWatcher();
 
-    //Figure out why this test fails
     var error;
     var callback = function(err) {
       if (err) {
@@ -37,10 +36,77 @@ describe('watcher', function () {
 
     waitsFor(function () {
       return error;
-    }, 'error was not set', 200);
+    }, 'error to be set', 200);
 
     runs(function() {
       expect(error).toMatch(/No such file or directory/);
+    });
+  });
+  it('addFile should call back without an error if path is found', function () {
+    var s = require('../lib/watcher').makeWatcher();
+
+    var done;
+    var callback = function(err) {
+      if (!err) {
+        done = true;
+      }
+    };
+
+    var path = require('path');
+    var tPath = path.resolve('.');
+    s.addFile(tPath, callback);
+
+    waitsFor(function () {
+      return done;
+    }, 'callback', 200);
+
+    runs(function() {
+      expect(done).toEqual(true);
+    });
+  });
+  it('checkFile should respond with true once a file is added', function () {
+    var s = require('../lib/watcher').makeWatcher();
+
+    var result;
+    var callback = function(err, r) {
+      if (!err) {
+        result = r;
+      }
+    };
+
+    var path = require('path');
+    var tPath = path.resolve('.');
+    s.addFile(tPath, function() {
+      s.checkFile(tPath, callback);
+    });
+
+    waitsFor(function () {
+      return result;
+    }, 'callback', 200);
+
+    runs(function() {
+      expect(result).toEqual(true);
+    });
+  });
+  it('checkFile should respond with false if a file has not been added', function () {
+    var s = require('../lib/watcher').makeWatcher();
+
+    var path = require('path');
+    var tPath = path.resolve('.');
+
+    var result = null;
+    s.checkFile(tPath, function(err, r) {
+      if (!err) {
+        result = r;
+      }
+    });
+
+    waitsFor(function () {
+      return (result !== null);
+    }, 'callback', 200);
+
+    runs(function() {
+      expect(result).toEqual(false);
     });
   });
 });
