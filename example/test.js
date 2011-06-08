c7dfb83f5bf875c1ca0acab134abafc4b2d8a56b
Justin Slattery <justin.slattery@fzysqr.com>
2011-06-07 22:14:41 -0700

    Fixed a bug that prevented folder add/remove/add from working. Removed all logging.

---
+++ example/test.js
@@ -0,0 +1,10 @@
+stalker = require('../');
+
+stalker.watch('./example', function(err, f) { 
+  if (err) {
+    console.log('Error was ' + err)
+    return;
+  }
+  console.log('I see ' + f); 
+});
+
